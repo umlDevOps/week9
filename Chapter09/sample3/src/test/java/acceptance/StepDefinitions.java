cb0a0a0fd1a068abbcb82e43a623923502bdf607
umlDevOps <124225814+umlDevOps@users.noreply.github.com>
2023-03-28 00:35:55 -0400

    Update StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -35,3 +35,10 @@ public class StepDefinitions {
         assertEquals(expectedResult, result);
     }
 }
+/*
+Feature: Calculator
+  Scenario: Sum two numbers
+    Given I have two numbers: 1 and 2
+    When the calculator sums them
+    Then I receive 3 as a result
+    */
